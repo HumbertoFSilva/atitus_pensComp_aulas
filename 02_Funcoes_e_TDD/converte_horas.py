17f3b7e0d4316b0aec4eb7d3e525d4a7d1afbd5a
HumbertoFSilva <humbertofsilva14@gmail.com>
2025-03-19 19:42:21 -0300

    horas_main

--- 02_Funcoes_e_TDD/converte_horas.py
+++ 02_Funcoes_e_TDD/converte_horas.py
@@ -12,17 +12,17 @@ def hora_para_segundo(valor):
     # TODO: preencher, deve usar minuto_para_segundo()
     pass
 
+def test():
+    assert hora_para_minuto(0) == 0
+    assert hora_para_minuto(1) == 60
+    assert hora_para_minuto(2) == 120
 
-assert hora_para_minuto(0) == 0
-assert hora_para_minuto(1) == 60
-assert hora_para_minuto(2) == 120
+    assert minuto_para_segundo(0) == 0
+    assert minuto_para_segundo(1) == 60
+    assert minuto_para_segundo(2) == 120
 
-assert minuto_para_segundo(0) == 0
-assert minuto_para_segundo(1) == 60
-assert minuto_para_segundo(2) == 120
+    assert hora_para_segundo(0) == 0
+    assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
+    assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
 
-assert hora_para_segundo(0) == 0
-assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
-assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
-
-print("Terminou com sucesso!")
+    print("Terminou")
