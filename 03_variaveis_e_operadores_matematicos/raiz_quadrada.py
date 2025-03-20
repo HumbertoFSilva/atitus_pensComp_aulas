c841d2f5f7e33ab5a125d50bb60cdaa05f98ee6e
HumbertoFSilva <humbertofsilva14@gmail.com>
2025-03-19 22:11:51 -0300

    raiz_main

--- 03_variaveis_e_operadores_matematicos/raiz_quadrada.py
+++ 03_variaveis_e_operadores_matematicos/raiz_quadrada.py
@@ -1,8 +1,11 @@
 def raiz_quadrada(valor):
-    # Preencher
+    return valor ** (1/2)
     pass
 
+def test():
+    assert raiz_quadrada(9) == 3
+    assert raiz_quadrada(16) == 4
+    assert raiz_quadrada(25) == 5
+    assert raiz_quadrada (49) == 7
 
-assert raiz_quadrada(9) == 3
-assert raiz_quadrada(16) == 4
-assert raiz_quadrada(25) == 5
+    print(raiz_quadrada(81))
