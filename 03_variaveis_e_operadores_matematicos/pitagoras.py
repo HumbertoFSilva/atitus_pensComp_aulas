f837ec819a2cb36a45cafec29ff8d569f9075cbe
HumbertoFSilva <humbertofsilva14@gmail.com>
2025-03-19 22:06:21 -0300

    pitagoras_main

--- 03_variaveis_e_operadores_matematicos/pitagoras.py
+++ 03_variaveis_e_operadores_matematicos/pitagoras.py
@@ -1,26 +1,28 @@
 def area(largura, altura):
-    # Preencher
-    pass
+    return largura * altura
 
 
 def perimetro(largura, altura):
-    # Preencher
-    pass
+    return largura + altura * 2
 
 
 def hipotenusa(largura, altura):
-    # Preencher
-    pass
+    return  largura * largura + altura * altura
+    
 
+def test():
+    assert area(5, 5) == 25
+    assert area(0, 5) == 0
+    assert area(10, 5) == 50
 
-assert area(5, 5) == 25
-assert area(0, 5) == 0
-assert area(10, 5) == 50
+    assert perimetro(2, 3) == 10
+    assert perimetro(5, 5) == 20
+    assert perimetro(10, 5) == 30
 
-assert perimetro(2, 3) == 10
-assert perimetro(5, 5) == 20
-assert perimetro(10, 5) == 30
+    assert hipotenusa(4, 3) == 5
+    assert hipotenusa(6, 8) == 10
+    assert hipotenusa(5, 12) == 13
 
-assert hipotenusa(4, 3) == 5
-assert hipotenusa(6, 8) == 10
-assert hipotenusa(5, 12) == 13
+print(area(20, 20))
+print(perimetro(3, 4))
+print(hipotenusa(5, 2))
